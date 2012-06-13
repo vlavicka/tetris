ea55c7e19fcd89ee2c14353c737de316796e5da8
Vladimir Lavicka <vladimir.lavicka@gmail.com>
2012-06-13 23:08:28 +0200

    Create playground and its dumps

--- playground.py
+++ playground.py
@@ -1,4 +1,47 @@
 from tools import area
 
 class PlayGroundModel(object):
-    pass
+    EMPTY_TILE = 0
+    def __init__(self, sx, sy):
+        self._area = area.Area(sx, sy, PlayGroundModel.EMPTY_TILE)
+
+    @property
+    def width(self):
+        return self._area.width
+
+    @property
+    def height(self):
+        return self._area.height
+
+    def set_color(self, x, y, value):
+        self._area[x, y] = value
+    
+    def get_color(self, x, y):
+        return self._area[x, y]
+
+    def get_filled(self):
+        result = []
+        for y in range(self._area.height):
+            is_filled = True
+            for x in range(self._area.width):
+                if self._area[x, y] == PlayGroundModel.EMPTY_TILE:
+                    is_filled = False
+                    break
+            if is_filled:
+                result.append(y)
+        return result
+
+    def dump_str(self):
+        result = []
+        result.append('+' + '-' * self._area.width + '+')
+        for y in range(self._area.height):
+            line = ''
+            for x in range(self._area.width):
+                line += ' ' if self._area[x, y] == PlayGroundModel.EMPTY_TILE else 'x'
+            result.append('|' + line + '|')
+        result.append('+' + '-' * self._area.width + '+')
+        return '\n'.join(result)
+
+    def dump_console(self):
+        """ Dump playground into console - output is colored """
+        raise NotImplementedError
--- tests/playground_test.py
+++ tests/playground_test.py
@@ -3,7 +3,7 @@ import unittest
 sys.path.append('..')
 
 from tools import testcommon
-import playground
+from playground import *
 
 # TODO: playground representation -> 2D array MxN with information about
 #       empty and filled tiles
@@ -14,20 +14,58 @@ import playground
 # TODO: drop current piece
 # TODO: dump playground on screen (colored) or into list of strings
 
+empty_playground = """\
++----+
+|    |
+|    |
+|    |
+|    |
+|    |
+|    |
+|    |
+|    |
++----+"""
+
+
 
 class TestPlayGroundModel(unittest.TestCase):
+    def setUp(self):
+        self.model = PlayGroundModel(4, 8)
+
     def test_create_playground(self):
         """ create playground """
-        pass
+        self.assertEqual(4, self.model.width)
+        self.assertEqual(8, self.model.height)
+        self.assertEqual(0, sum(self.model._area))
 
+    def test_dump_playground(self):
+        """ dump playground into string """
+        self.assertEqual(empty_playground, self.model.dump_str())
         
-    # TEST: dump playground into string
-    # TEST: set tile to specific color
-    # TEST: dump colored playground onto screen
+    def test_set_tile(self):
+        """ set tile to specific color """
+        self.model.set_color(0, 0, 1)
+        self.assertEqual(1, self.model.get_color(0, 0))
+        
+    def test_check_filled_lines(self):
+        """ check for filled lines """
+        self.model.set_color(2, 6, 1)
+        for i in range(4):
+            self.model.set_color(i, 7, 1)
+        self.assertEqual([7], self.model.get_filled())
+
+    def test_check_filled_lines_multi(self):
+        """ check for filled lines """
+        self.model.set_color(2, 6, 1)
+        for i in range(4):
+            self.model.set_color(i, 7, 1)
+            self.model.set_color(i, 5, 1)
+        self.assertEqual([5, 7], self.model.get_filled())
 
 
-    # TEST: check for filled lines
     # TEST: remove filled lines
+
+    # TEST: dump colored playground onto screen
 
 if __name__ == "__main__":
     testcommon.run_unit_tests(globals())
